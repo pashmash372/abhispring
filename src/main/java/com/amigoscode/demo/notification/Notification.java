5bba0ae3c260267cff59c0d5238835a849a8becc
pashmash372 <abhilashsivasanker@gmail.com>
2020-01-19 05:11:42 +0530

    first

---
+++ src/main/java/com/amigoscode/demo/notification/Notification.java
@@ -0,0 +1,56 @@
+package com.amigoscode.demo.notification;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+public class Notification {
+    private String messages;
+    private ZonedDateTime timeStamp;
+
+    public Notification(String messages, ZonedDateTime timeStamp) {
+        this.messages = messages;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessages() {
+        return messages;
+    }
+
+    public void setMessages(String messages) {
+        this.messages = messages;
+    }
+
+    public ZonedDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(ZonedDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Notification() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "messages='" + messages + '\'' +
+                ", timeStamp=" + timeStamp +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notification that = (Notification) o;
+        return messages.equals(that.messages) &&
+                timeStamp.equals(that.timeStamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messages, timeStamp);
+    }
+}
